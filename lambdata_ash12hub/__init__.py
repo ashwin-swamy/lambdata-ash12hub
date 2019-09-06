0713676bcf6e61920497bc55f34d8768e87142b2
ash12hub <ash12hub@users.noreply.github.com>
2019-09-06 10:27:01 +0530

    updated split_date_list and add_new_column methods

--- lambdata_ash12hub/__init__.py
+++ lambdata_ash12hub/__init__.py
@@ -4,7 +4,6 @@ lambdata-ash12hub - A Collection of Data Science helper functions.
 
 import pandas as pd;
 import numpy as np;
-# from sklearn.model_selection import train_test_split;
 
 def train_validation_test_split(x, y, train_size=0.8, val_size=0.1, test_size=0.1, random_state=None, shuffle=True):
     assert train_size + val_size + test_size == 1;
@@ -14,15 +13,13 @@ def train_validation_test_split(x, y, train_size=0.8, val_size=0.1, test_size=0.
     x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size=val_size/(train_size+val_size), random_state=random_state, shuffle=shuffle);
     return x_train, x_val, x_test, y_train, y_val, y_test;
 
-def split_date(df, date_column):
-    df = df.copy();
-    df[date_column] = pd.to_datetime(df[date_column], infer_datetime_format=True);
+def split_date_list(date_list):
+    date_list = pd.to_datetime(date_list, infer_datetime_format=True);
+    day = date_list.dt.day;
+    month = date_list.dt.month;
+    year = date_list.dt.year;
+    return day, month, year;
 
-    df['date'] = df[date_column].dt.date;
-    df['month'] = df[date_column].dt.month;
-    df['year'] = df[date_column].dt.year;
-    return df['date'], df['month'], df['year'];
-
-def add_list_as_new_column(data_frame, new_list, new_list_name):
-    data_frame[new_list_name] = new_list;
-    return;
+def add_new_column(new_list):
+    new_column = pd.Series(new_list);
+    return new_column;
